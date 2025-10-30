087ecef8563a6296f5ab420fc799bfc499befc04
LCluber <ludovic.cluber@gmail.com>
2025-10-30 09:37:55 +0100

    Refactor gatlin-express.d.ts: remove unused middleware functions and retain getConsumer

--- src/gatlin-express.d.ts
+++ src/gatlin-express.d.ts
@@ -1,24 +1,9 @@
 import type { Request, Response, NextFunction } from 'express';
 
-// Extend Express Request interface globally
-declare global {
-  namespace Express {
-    interface Request {
-      isProtected?: boolean;
-      decodedAccessToken?: any;
-      decodedRefreshToken?: any;
-    }
-  }
-}
-
-declare function refresh(req: Request, res: Response, next: NextFunction): Promise<void>;
-declare function decodeAccess(req: Request, _res: Response, next: NextFunction): void;
-declare function decodeRefresh(req: Request, _res: Response, next: NextFunction): Promise<void>;
+declare function getConsumer(req: Request, res: Response, next: NextFunction): Promise<void>;
 
 export { 
-  refresh,
-  decodeAccess,
-  decodeRefresh,
+  getConsumer,
 };
 
 
